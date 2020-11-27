8183868e338c3ad212d23354b0532bce6093b4e2
RT503 <musinspiration@yandex.ru>
2020-11-27 05:42:18 +0300

    js1streviev

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,9 @@ let popupTitle = document.querySelector('.popup__title');
 let popupContent = document.querySelector('.popup__content');
 let popupCloseButton = document.querySelector('.popup__close');
 let editButton = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.popup__form');
+let form = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let statusInput = document.querySelector('.popup__input_type_status');
-let popupSubmitButton = document.querySelector('.popup__submit-button');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
@@ -24,8 +23,9 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileStatus.textContent = statusInput.value;
+  closePopup();
 }
 
 editButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', closePopup);
-popupSubmitButton.addEventListener('submit', formSubmitHandler);
+form.addEventListener('submit', formSubmitHandler);
